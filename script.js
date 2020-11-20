630550bdc69837dafce5cf1be0efa057f5c99ff4
Aril-Sa <arisahiti@gmail.com>
2020-11-20 23:26:05 +0100

    Subscribe done

---
+++ script.js
@@ -0,0 +1,18 @@
+/* Form Subscribe */
+let date = new Date();
+document.getElementById("dTime").innerHTML = date;
+
+/* function myFunction() {
+    let fName = document.getElementById("fName").value;
+    let email = document.getElementById("email").value;
+    document.getElementById("display").innerHTML = 
+    fName  + " your email " + email +  " has been addet to the subscription list!"; 
+} */
+
+
+function lightUp(){
+    document.getElementsByClassName("butt").style.backgroundColor = "red";
+};
+
+
+
