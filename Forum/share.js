8968557be60d5983ec69abca4a26f0df33efb543
Aril-Sa <arisahiti@gmail.com>
2020-11-25 08:24:20 +0100

    Forum added

---
+++ Forum/share.js
@@ -0,0 +1,41 @@
+
+let content = document.getElementById("content");
+let button = document.getElementById("show_more");
+
+
+button.onclick = function() {
+    if(content.className == "open") {
+        /* Shrink box */
+        content.className = "";
+        button.innerHTML = "Show More";
+    } else {
+        /* Expand box */
+        content.className = "open";
+        button.innerHTML = "Show Less";
+    }
+};
+
+/* function myFunction1() {
+    let textArea = document.getElementById("text_area").value;
+    document.getElementById("content").innerHTML = textArea;
+    let nameSubmit = document.getElementById("name_submit").value;
+    document.getElementById("person_message").innerHTML = nameSubmit + " commented.";   
+    
+    if(nameSubmit == ""){
+        document.getElementById("person_message").innerHTML = "Anonymous comment.";
+    }
+} */
+
+function myFunction1(){
+    let newText =  document.createElement("section");
+    let textArea = document.getElementById("text_area").value;
+    let nameSubmit = document.getElementById("name_submit").value;
+    document.getElementById("person_message").innerHTML = nameSubmit + " commented."; 
+    document.getElementById("content").innerHTML = textArea;
+    document.getElementById("content").appendChild(newText);
+    
+    if(nameSubmit == ""){
+        document.getElementById("person_message").innerHTML = "Anonymous comment.";
+    }
+    
+}
--- script.js
+++ script.js
@@ -9,12 +9,16 @@ document.getElementById("dTime").innerHTML = date;
     let email = document.getElementById("email").value;
     document.getElementById("display").innerHTML = 
     fName  + " your email " + email +  " has been addet to the subscription list!"; 
-} */
+}
 
 
-function lightUp(){
-    document.getElementsByClassName("butt").style.backgroundColor = "red";
-};
+const toggleButten = document.getElementsByClassName("toggle_button")[0]
+const navLink = document.getElementsByClassName("nav_link")[0]
+
+toggleButten.addEventListener("click", () => {
+    navLink.classList.toggle("active");
+})
+
 
 
 
